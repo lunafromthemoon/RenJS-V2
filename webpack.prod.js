27a8e532b65af71f6aebc8869c1787e5b72aec1c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-05 00:22:38 -0400

    remove unnecessary prod overrides

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,6 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
-const path = require('path');
-
-var PACKAGE = require('./package.json');
-var version = PACKAGE.version;
 
 module.exports = merge(common, {
     mode: 'production',
-    output: {
-        filename: `[name]-v${version}.js`,
-        path: path.resolve(__dirname, 'dist'),
-        library: 'RenJS',
-        libraryTarget: "var",
-        globalObject: "this"
-    }
 })
