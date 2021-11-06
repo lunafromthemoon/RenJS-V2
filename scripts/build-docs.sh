cc8cc5aa862c7f4169bef2876a2b174688d06f20
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-06 13:29:48 -0400

    update quickstart as part of docs build

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,3 +4,5 @@ VERSION=`node -p "require('./package.json').version"`
 
 cp dist/renjs.js docs/downloads/releases/renjs-v$VERSION.js
 cp dist/renjs.js docs/downloads/releases/renjs.js
+cd RenJSQuickstart_V2
+bestzip ../docs/downloads/RenJSQuickstart_V2.zip *
