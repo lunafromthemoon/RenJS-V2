5c39738730cf520853c8b45b55b18bee6fa55688
lunafromthemoon <je.luna.mdq@gmail.com>
2021-11-08 18:35:04 +0100

    test multiple boxes

---
+++ dev-only/story/multipleboxes/boot.js
@@ -0,0 +1,32 @@
+const globalConfig =  {
+  'name': 'Quickstart',
+  'w': 800,
+  'h': 600,
+  'renderer': Phaser.AUTO, // become renderer
+  'scaleMode': Phaser.ScaleManager.SHOW_ALL,
+  'loadingScreen': {
+    'background': 'assets/gui/quickstartbg.png',
+    'loadingBar': {
+      'asset': 'assets/gui/loaderloading-bar.png',
+      'position': {
+        'x': 109,
+        'y': 458
+      },
+      'size': {
+        'w': 578,
+        'h': 82
+      }
+    }
+  },
+  'logChoices': true,
+  'fonts': 'assets/gui/fonts.css',
+  'guiConfig': 'story/multipleboxes/GUI.yaml',
+  'storyConfig': 'story/Config.yaml',
+  storySetup: 'story/multipleboxes/Setup.yaml',
+  'storyText': [
+    'story/multipleboxes/Story.yaml'
+  ]
+}
+
+const RenJSGame = new RenJS.game(globalConfig)
+RenJSGame.launch()
