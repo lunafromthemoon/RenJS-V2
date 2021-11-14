d98a286b9b02f01904a3acaf492d4636369c3fd0
lunafromthemoon <je.luna.mdq@gmail.com>
2021-11-14 11:41:10 +0100

    Update quickstart build

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,5 +4,6 @@ VERSION=`node -p "require('./package.json').version"`
 
 cp dist/renjs.js docs/downloads/releases/renjs-v$VERSION.js
 cp dist/renjs.js docs/downloads/releases/renjs.js
+cp dist/renjs.js RenJSQuickstart_V2/renjs.js
 cd RenJSQuickstart_V2
 bestzip ../docs/downloads/RenJSQuickstart_V2.zip *
