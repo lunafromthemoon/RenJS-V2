25821f87ea791fb94f8892ec2c3568bffe96857a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-12-02 23:43:43 -0500

    chore: remove unused import

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,3 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const path = require('path')
